669d916474824659090d88e65e12fd5c27ddb18a
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-06-27 09:08:20 -0300

    estrutura de pastas e organização de arquivos

---
+++ src/styles/themes/theme1.js
@@ -0,0 +1,23 @@
+// ### Theme 1
+
+// #### Backgrounds
+
+// --very-dark-desaturated-blue: hsl(222, 26%, 31%); /* (main background) */
+// --very-dark-desaturated-blue: hsl(223, 31%, 20%); /* (toggle background, keypad background) */
+// --very-dark-desaturated-blue: hsl(224, 36%, 15%); /* (screen background)*/
+
+// #### Keys
+
+// --desaturated-dark-blue: hsl(225, 21%, 49%); /* (key background) */
+// --desaturated-dark-blue: hsl(224, 28%, 35%) ; /* (key shadow) */
+
+// --red: hsl(6, 63%, 50%); /* (key background, toggle) */
+// --dark-red: hsl(6, 70%, 34%); /* (key shadow) */
+
+// - Light grayish orange (key background): hsl(30, 25%, 89%)
+// - Grayish orange (key shadow): hsl(28, 16%, 65%)
+
+// #### Text
+
+// - Very dark grayish blue: hsl(221, 14%, 31%)
+// - White: hsl(0, 0, 100%)
---
+++ src/styles/themes/theme2.js
@@ -0,0 +1,23 @@
+// ### Theme 2
+
+// #### Backgrounds
+
+// - Light gray (main background): hsl(0, 0%, 90%)
+// - Grayish red (toggle background, keypad background): hsl(0, 5%, 81%)
+// - Very light gray (screen background): hsl(0, 0%, 93%)
+
+// #### Keys
+
+// - Dark moderate cyan (key background): hsl(185, 42%, 37%)
+// - Very dark cyan (key shadow): hsl(185, 58%, 25%)
+
+// - Orange (key background, toggle): hsl(25, 98%, 40%)
+// - Dark orange (key shadow): hsl(25, 99%, 27%)
+
+// - Light grayish yellow (key background): hsl(45, 7%, 89%)
+// - Dark grayish orange (key shadow): hsl(35, 11%, 61%)
+
+// #### Text
+
+// - Very dark grayish yellow: hsl(60, 10%, 19%)
+// - White (text): hsl(0, 0, 100%)
